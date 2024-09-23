60171c356a2e4047d2a43a43446316d4b83daa1d
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-09-23 23:23:54 +0600

    Dashboard updated coding done

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,18 +1,31 @@
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 const Dashboard = () => {
   const { data: session } = useSession();
+  console.log(session);
 
   return (
     <div>
-      {session ? (
-        <div>
-          <h1>Welcome, {session.user?.name}</h1>
-          <p>Your access token: {session.accessToken}</p>
-          <p>Your email: {session.user?.email}</p>
-        </div>
-      ) : (
-        <p>Please log in to view your dashboard.</p>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Logged-in user email: {session?.user?.email}
+          </h1>
+          <Image
+            src={
+              session?.user?.image ||
+              "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
+            }
+            width={100}
+            height={100}
+            alt="user image"
+            className="mx-auto rounded-full mt-5"
+          />
+        </>
       )}
     </div>
   );
